e90ee6761d84b86c880613841ff49efb454c0e93
liu770807152 <liu770807152@hotmail.com>
2020-05-06 17:35:21 +1000

    No.6 updated

---
+++ courses/6/question.js
@@ -0,0 +1,25 @@
+var o1 = {
+    text: 'o1',
+    fn: function() {
+        return this.text;
+    }
+}
+
+var o2 = {
+    text: 'o2',
+    fn: function() {
+        return o1.fn();
+    }
+}
+
+var o3 = {
+    text: 'o3',
+    fn: function() {
+        var fn = o1.fn;
+        return fn();
+    },
+};
+
+console.log(o1.fn()); //o1
+console.log(o2.fn()); //11行决定是o1
+console.log(o3.fn()); //undefined
