4cb370b793510c76a651d12d01a909d6da7fbc12
liu770807152 <liu770807152@hotmail.com>
2020-07-06 19:11:38 +1000

    re-organized

--- courses/7/npm/function.js
+++
@@ -1,8 +0,0 @@
-// (function (exports, require, module, __filename, __dirname) {
-const appStart = require("./timestamp");
-console.log(__filename, __dirname);
-appStart();
-setTimeout(() => {
-  timestamp();
-}, 1000);
-// })
