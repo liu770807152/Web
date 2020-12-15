050c296c69d766edfa3205a5056225242346d8dd
liu770807152 <liu770807152@hotmail.com>
2020-12-16 00:37:29 +1100

    🎉 Now I understand currying

--- Javascript_tutorial/currying.js
+++ Javascript_tutorial/currying.js
@@ -1,12 +1,12 @@
 let adder = function() {
   let _args = Array.prototype.slice.call(arguments);
-  console.log("line 3 ~ add ~ arguments", arguments)
-  console.log("line 3 ~ add ~ _args", _args)
+  console.log("line 3 ~ add ~ arguments", arguments);
+  console.log("line 3 ~ add ~ _args", _args);
 
   let _addNum = function() {
     _args.push(...arguments);
-    console.log("line 7 ~ add ~ _args", _args)
-    console.log("line 7 ~ add ~ arg", ...arguments)
+    // console.log("line 7 ~ add ~ arg", ...arguments)
+    // console.log("line 7 ~ add ~ _args", _args)
     return _addNum;
   }
 
@@ -18,18 +18,18 @@ let adder = function() {
 
   return _addNum;
 }
-console.log(adder(3,5)(2)(10)());
+console.log(adder(3,5)(2)(10,0)());
 
 
 let adder2 = function(...nums) {
   let _args = nums;
-  console.log("line 3 ~ add ~ nums", nums)
-  console.log("line 3 ~ add ~ _args", _args)
+  console.log("line 3 ~ add ~ nums", nums);
+  console.log("line 3 ~ add ~ _args", _args);
 
-  let _addNum = function(...nums) {
-    _args = _args.concat(nums);
-    console.log("line 7 ~ add ~ _args", _args)
-    console.log("line 7 ~ add ~ nums", nums)
+  let _addNum = function(...x) {
+    _args = _args.concat(x);
+    // console.log("line 7 ~ add ~ x", x)
+    // console.log("line 7 ~ add ~ _args", _args)
     return _addNum;
   }
 
@@ -41,4 +41,30 @@ let adder2 = function(...nums) {
 
   return _addNum;
 }
-console.log(adder2(3,5)(2)(10)());
+console.log(adder2(3,5)(2)(10,0)());
+
+let adder3 = function(...nums) {
+  function add(x) {
+    let sum = 0;
+    x.forEach(x => {
+      sum += x;
+    });
+    return sum;
+  }
+
+  let _result = add(nums);
+  console.log("line 55 ~ adder3 ~ _result", _result);
+
+  let _addNum = function(...x) {
+    _result += add(x);
+    console.log("line 58 ~ adder3 ~ _result", _result);
+    return _addNum;
+  }
+
+  _addNum.toString = function() {
+    return _result;
+  }
+
+  return _addNum;
+}
+console.log(adder3(3,5)(2)(10,0)());
