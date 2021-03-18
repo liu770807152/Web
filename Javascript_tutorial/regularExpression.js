d7362271723b6449c8be148ef3480910e8003b12
liu770807152 <liu770807152@hotmail.com>
2021-03-18 17:27:09 +1100

    add more questions from interviews

--- Javascript_tutorial/regularExpression.js
+++ Javascript_tutorial/regularExpression.js
@@ -1,6 +1,9 @@
 /* =======================1. Match Principle========================== */
 // 匹配成功，从成功匹配的子字符串的下个字符开始匹配，能匹配多少就匹配多少（默认贪婪模式）；
 // 匹配失败，则从当前字符的下个字符开始匹配
+var str2 = '0987r123';
+var reg4 = /([1234567890A-z])[123456789|0][1234567890]/g; // |表逻辑或, ()提高优先级, ^表逻辑非
+str2.match(reg4);  // ->  ["098", "r12"], 没有987和123！
 
 /* =======================2. Simple Use========================== */
 var str1 = 'This is a test.\nTest is completed.';
@@ -25,6 +28,57 @@ str = 'aabbcc';
 reg.exec(str);
 // 3. 对于match()和exec(), 如果正则表达式包含子表达式，则返回结果的类数组中还会捕获子表达式的匹配结果。
 //    如果不想捕获某些子表达式的结果，则需要在那些子表达式的开头加上'?:'，如(?:a)(b)(c)的结果就没有"a"
+
+/* =======================4. 面试题========================== */
+// 1. 将xxyy变成yyxx
+var str = 'aabbccdd';
+reg = /(\w)\1(\w)\2/g;
+// "bbaaddcc", 用$N拿匹配结果中的第N个子表达式
+// 对于"aabb", $1是a，$2是b
+str.replace(reg, $2$2$1$1);  
+
+// 同理
+str.replace(reg, function($, $1, $2) {
+  console.log($, $1, $2);
+  // ->  "aabb" "a" "b", 因为有了()，用了子表达式做了分类，才是这个结果
+  return $2 + $2 + $1 + $1;
+});
+
+// 2.1 将指定单词的首字母大写
+var str = 'js-plus-plus';
+reg = /-(\w)/g;
+str.replace(reg, function($, $1) {
+  return $1.toUpperCase();
+});
+// 2.2 匹配所有整数，包括正数和负数
+reg = /([-?]\d+)/g;
+
+// 3 在指定单词前面添加下划线，如大写开头的单词
+var str = 'jsPlusPlus';
+reg = /(A-Z)/g // 或 /(\W)/g
+str.replace(reg, function($, $1) {
+  return '_' + $1.toLowerCase();
+});
+
+// 4.1 把xxyyzz变成XxYyZz
+var str = 'xxyyzz';
+reg = /(\w)\1(\w)\2(\w)\3/g
+str.replace(reg, function($, $1, $2, $3) {
+  return $1.toUpperCase() + $1 + $2.toUpperCase() + $2 + $3.toUpperCase() + $3;
+});
+// 4.2 把aabbcc变成a$b$c$, 不能使用函数
+var str = 'aabbcc';
+reg = /(\w)\1(\w)\2(\w)\3/g
+// $1$$$2$$$3$$拆解成$1 $$ $2 $$ $3 $$
+// 如果要替换成'$', 要在前面再加一个'$'特殊表示，其他符号不用
+// 匹配转义字符就在前面加'\', 如'?'、'*'、'+'
+str.replace(reg, '$1$$$2$$$3$$');
+
+// 5. 把含有重复字符的字符串去重
+var str = 'aaabbcccccccccc';
+reg = /(\w)\1*/g
+str.replace(reg, '$1');
+
 
 
 var input1 = 'ABCDa2!', input2 = 'Aa21#', input3 = 'aa!';
