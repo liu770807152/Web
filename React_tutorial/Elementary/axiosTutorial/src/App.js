0142de79bcc5a94110aa72f8f3e42d6c9c5a135c
liu770807152 <liu770807152@hotmail.com>
2021-05-19 22:01:39 +1000

    add proxy to access 2 servers

--- React_tutorial/Elementary/axiosTutorial/src/App.js
+++ React_tutorial/Elementary/axiosTutorial/src/App.js
@@ -5,7 +5,18 @@ import "./App.css";
 class App extends Component {
   getStudentData = () => {
     // port number is 3000 rather than 5000 because we use proxy!
-    axios.get("http://localhost:3000/students").then(
+    axios.get("http://localhost:3000/api1/students").then(
+      (res) => {
+        console.log(res.data);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  };
+
+  getCarData = () => {
+    axios.get("http://localhost:3000/api2/cars").then(
       (res) => {
         console.log(res.data);
       },
@@ -19,6 +30,7 @@ class App extends Component {
     return (
       <div>
         <button onClick={this.getStudentData}>Click me to get student data</button>
+        <button onClick={this.getCarData}>Click me to get car data</button>
       </div>
     );
   }
