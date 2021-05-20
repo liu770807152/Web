df72622e6d8294d06e7eb2b2fde9bdd0ca84b242
liu770807152 <liu770807152@hotmail.com>
2021-05-20 17:35:25 +1000

    add a new way to access data using Fetch

--- React_tutorial/Elementary/github-search-pubsub/src/components/Search/index.jsx
+++ React_tutorial/Elementary/github-search-pubsub/src/components/Search/index.jsx
@@ -1,20 +1,54 @@
 import React, { Component } from "react";
+import PubSub from 'pubsub-js';
 import './index.css';
 import axios from 'axios';
 
 export default class Search extends Component {
 
-  search = () => {
+  search = async () => {
     // continuous destructuring assignment + rename variable
     const { keywordElement: { value: name } } = this;
     // update state before sending the request
-    this.props.updateAppState({ isFirst: false, isLoading: true });
-    // send the request
+    PubSub.publish('message', { isFirst: false, isLoading: true });
+    // use Axios to send the request
     axios.get(`/api/search/users?q=${name}`).then(res => {
-      this.props.updateAppState({ isLoading: false, users: res.data.items });
+      PubSub.publish('message', { isLoading: false, users: res.data.items });
     }, err => {
-      this.props.updateAppState({ isLoading: false, err: err.message });
+      PubSub.publish('message', { isLoading: false, err: err.message });
     });
+
+    // use Fetch to send the request
+    /*fetch(`/api/search/users?q=${name}`).then(res => {
+      console.log('Communication success!'); 
+      if (res.ok) {
+        return res.json(); // res.json() contains a Promise instance
+      } else {
+        throw new Error(`HTTP status code exception: ${res.status}`);
+      }
+    }, err => {
+      console.error('Communication failure! ', err);
+      return new Promise(); // return an initial Promise instance to stop the chain
+    }).then(res => {
+      PubSub.publish('message', { isLoading: false, users: res.items });
+    }).catch(err => {
+      console.error('Communication failure! ', err);
+      PubSub.publish('message', { isLoading: false, err: err.message });
+    });*/
+
+    // use async & await
+    /*try {
+      const res = await fetch(`/api/search/users?q=${name}`);
+      if (res.ok) {
+        const data = res.json();
+        PubSub.publish('message', { isLoading: false, users: data.items });
+      } else {
+        throw new Error(`HTTP status code exception: ${res.status}`);
+      }
+    } catch (err) {
+      console.error('Communication failure! ', err);
+      PubSub.publish('message', { isLoading: false, err: err.message });
+    }*/
+    
   }
 
   render() {
