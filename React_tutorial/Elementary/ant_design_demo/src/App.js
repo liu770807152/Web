7b39fe608d6dc41e51f4a670cce83a7a92dda76b
liu770807152 <liu770807152@hotmail.com>
2021-06-02 23:22:55 +1000

    add a new tutorial of ant-design demo

---
+++ React_tutorial/Elementary/ant_design_demo/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <>
+      <button>default button</button>
+      <Button type="primary">primary button</Button>
+    </>
+  );
+}
+
+export default App;
