0b9e276e9a2f71f993560cb4edacb030f638fae1
liu770807152 <liu770807152@hotmail.com>
2021-06-11 01:26:50 +1000

    finalize simple express demo

--- Node_tutorial/Express_Tutorial/simple_demo/app.js
+++ Node_tutorial/Express_Tutorial/simple_demo/app.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const fs = require('fs');
-const { getDB } = require('./db');
+const express = require("express");
+const fs = require("fs");
+const { getDB, saveDB } = require("./db");
 
 const app = express();
 
-app.get('/todos', async (req, res) => {
+// set up parsing of application/json from req.body
+app.use(express.json());
+// set up parsing of application/x-www-form-urlencoded from req.body
+app.use(express.urlencoded());
+
+app.get("/todos", async (req, res) => {
   try {
     const db = await getDB();
     res.status(200).json(db.todos);
   } catch (err) {
     res.status(500).json({
-      error: err.message
+      error: err.message,
     });
   }
-  
+
   // fs.readFile('./db.json', 'utf8', (err, data) => {
   //   if (err) {
   //     return res.status(500).json({
@@ -25,10 +30,12 @@ app.get('/todos', async (req, res) => {
   // });
 });
 
-app.get('/todos', (req, res) => {
+app.get("/todos", async (req, res) => {
   try {
     const db = await getDB();
-    const todo = db.todos.find(todo => todo.id === Number.parseInt(req.params.id));
+    const todo = db.todos.find(
+      (todo) => todo.id === Number.parseInt(req.params.id)
+    );
     if (!todo) {
       res.status(404).end();
     } else {
@@ -36,7 +43,7 @@ app.get('/todos', (req, res) => {
     }
   } catch (err) {
     res.status(500).json({
-      error: err.message
+      error: err.message,
     });
   }
 
@@ -56,6 +63,77 @@ app.get('/todos', (req, res) => {
   // });
 });
 
-app.listen(3000, () => {
-  console.log('server running at http://localhost:3000/');
+app.post("/todos", async (req, res) => {
+  try {
+    // 1. get req.body
+    const todo = req.body;
+    // 2. validate data
+    if (!todo.title) {
+      return res.status(422).json({
+        error: 'The field "title" is required!',
+      });
+    }
+    // 3. store data into db
+    const db = await getDB();
+    const lastTodo = db.todos[db.todos.length - 1];
+    todo.id = lastTodo ? lastTodo.id + 1 : 1;
+    db.todos.push(todo);
+    await saveDB(db);
+    // 4. send response
+    res.status(201).json(todo);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.patch('/todos/:id', async (req, res) => {
+  try {
+    // 1. get req.body
+    const todo = req.body;
+    // 2. search db
+    const db = await getDB();
+    const result = db.todos.find(todo => todo.id === Number.parseInt(req.params.id));
+    if (!result) {
+      return res.status(404).end();
+    }
+    // 3. validate data
+    if (!todo.title) {
+      return res.status(422).json({
+        error: 'The field "title" is required!',
+      });
+    }
+    // 4. update database
+    Object.assign(result, todo);
+    await saveDB(db);
+    // 5. return response
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const todoId = Number.parseInt(req.params.id);
+    const db = getDB();
+    db.todos.findIndex(todo => todo.id === todoId);
+    if (index === -1) {
+      return res.status(404).end();
+    }
+    db.todos.splice(index, 1);
+    await saveDB(db);
+    res.status(204).end();
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
 })
+
+app.listen(3000, () => {
+  console.log("server running at http://localhost:3000/");
+});
--- Node_tutorial/Express_Tutorial/simple_demo/db.js
+++ Node_tutorial/Express_Tutorial/simple_demo/db.js
@@ -3,9 +3,16 @@ const path = require('path');
 const { promisify } = require('util');
 // add support of promise
 const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 const dbPath = path.join(__dirname, './db.json');
 
 exports.getDB = async () => {
   const data = await readFile(dbPath, 'utf8');
   return JSON.parse(data);
+}
+
+exports.saveDB = async (db) => {
+  // set indentation to double spaces
+  const data = JSON.stringify(db, null, '  ');
+  await writeFile(dbPath, data);
 }
